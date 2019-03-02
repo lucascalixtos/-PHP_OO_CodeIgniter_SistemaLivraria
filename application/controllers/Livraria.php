f30af91c535bf70fe8da8e18ba45126d7eeb0aad
Lucas Calixto <lucascalixto180@gmail.com>
2019-03-02 00:44:43 -0300

    Inicio do trabalho, modificações na pagina inicial

--- application/controllers/Livraria.php
+++ application/controllers/Livraria.php
@@ -6,7 +6,9 @@ class Livraria extends CI_Controller {
 	public function index(){
 		$this->load->view('common/header');
         $this->load->view('common/navbar');
-        $this->load->view('elementos/carrossel');
+        $v['carrossel'] = $this->load->view('elementos/carrossel','',true);
+        $v['texto'] = '<h1> Esse é o texto </h2> <p>Apenas um teste</p>';
+        $this->load->view('elementos/layout', $v);
 		$this->load->view('common/footer');
 	}
 }
---
+++ application/views/elementos/layout.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-md-12">
+        <?= $carrossel ?>
+    </div>
+</div>
+<div class="container mx-auto mt-4 text-center">
+    <div class="row wow fadeInUp">
+        <div class="col-md-12">
+            <?= $texto ?>
+        </div>
+    </div>
+</div>
